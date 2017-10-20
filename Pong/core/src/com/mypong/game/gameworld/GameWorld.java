27083d447f3e28424c32473bf2c149aaef4ff28f
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2017-10-21 01:36:47 +0200

    New bar every ten seconds
    New bars create every ten seconds work fine.

--- Pong/core/src/com/mypong/game/gameworld/GameWorld.java
+++ Pong/core/src/com/mypong/game/gameworld/GameWorld.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.mypong.game.gameobjects.Ball;
 import com.mypong.game.gameobjects.Bar;
 import java.util.LinkedList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GameWorld {
     private int screenWidth;
@@ -12,9 +15,15 @@ public class GameWorld {
     private LinkedList<Bar> bars;
     private Ball ball;
 
+    private int barHeight;
+    private int barWidth;
+    private int velocity;
+
     private ScrollHandler handler;
 
     private boolean gameOver;
+
+    private ScheduledExecutorService timer;
 
     public GameWorld() {
         newGame();
@@ -37,6 +46,7 @@ public class GameWorld {
 
         if(ball.getCollisionsBall() == 3){
             this.gameOver = true;
+            this.timer.shutdown();
             ball.onRelease();
 
             for (Bar bar : bars){
@@ -62,22 +72,31 @@ public class GameWorld {
         this.screenHeight = Gdx.graphics.getHeight();
         this.screenWidth = Gdx.graphics.getWidth();
 
-        int barHeight = this.screenHeight / 10;
-        int barWidth =  barHeight / 10;
+        this.barHeight = this.screenHeight / 10;
+        this.barWidth =  barHeight / 10;
 
         int halfHeight = this.screenHeight / 2;
         int halfWidth = this.screenWidth / 2;
 
         int ballWidth = this.screenWidth / 100;
 
-        int velocity = this.screenWidth / 4;
+        this.velocity = this.screenWidth / 4;
 
         this.gameOver = false;
 
-        bars.add(new Bar(barWidth, barHeight, velocity, bars));
-        bars.add(new Bar(barWidth, barHeight, velocity, bars));
+        this.bars.add(new Bar(barWidth, barHeight, velocity, bars));
+        this.bars.add(new Bar(barWidth, barHeight, velocity, bars));
 
-        ball = new Ball(halfWidth, halfHeight, ballWidth);
+        this.ball = new Ball(halfWidth, halfHeight, ballWidth);
+
+        final Runnable tarea = new Runnable() {
+            public void run() {
+                bars.add(new Bar(barWidth, barHeight, velocity, bars));
+            }
+        };
+
+        this.timer = Executors.newSingleThreadScheduledExecutor();
+        this.timer.scheduleAtFixedRate(tarea, 10, 10, TimeUnit.SECONDS);
     }
 
     public int getScreenWidth() {
